f056fb3da6d4ed53ee7ce483088b35d4ee7691c4
Preetika Mondal <preetikamon11@gmail.com>
2018-05-02 11:22:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,28 +7,69 @@ cal.title("Smart Calculator")
 txtDisplay = Entry(cal, font=('arial', 20, 'bold'), bd=30, insertwidth=4, bg="powder blue",
                              justify='right')
 
+
 class Calculator:
 
     def __init__(self):
-        self.result = 0
+        self.output = 0
         self.current = 0
         self.operator = ""
+        self.isButton = 0
+        self.isRightParen = 0
 
     def btnClick(self, num):
+        if self.isRightParen is 1:
+            self.operator = self.operator + "*" + str(num)
+            txtDisplay.delete(0, END)
+            txtDisplay.insert(0, self.operator)
+            self.expOutput(self.operator)
+            self.isButton = 1
+            self.isRightParen = 0
+
+        elif self.isRightParen is 0:
             self.operator = self.operator + str(num)
             txtDisplay.delete(0, END)
             txtDisplay.insert(0, self.operator)
             self.expOutput(self.operator)
+            self.isButton = 1
+            self.isRightParen = 0
+
+    def leftParenthesesClick(self):
+
+        if self.isRightParen is 1:
+            self.operator = self.operator + "*" + "("
+            txtDisplay.delete(0, END)
+            txtDisplay.insert(0, self.operator)
+            self.isRightParen = 0
+
+        elif self.isButton is 0:
+            self.operator = self.operator + "("
+            txtDisplay.delete(0, END)
+            txtDisplay.insert(0, self.operator)
+            self.isRightParen = 0
+        elif self.isButton is 1:
+            self.operator = self.operator + "*" + "("
+            txtDisplay.delete(0, END)
+            txtDisplay.insert(0, self.operator)
+            self.isRightParen = 0
+
+    def rightParenthesesClick(self):
+        self.operator = self.operator + ")"
+        txtDisplay.delete(0, END)
+        txtDisplay.insert(0, self.operator)
+        self.expOutput(self.operator)
+        self.isRightParen = 1
 
     def expOutput(self, operator):
         try:
-            self.result = str(eval(operator))
+            self.output = str(eval(operator))
             txtDisplay.delete(0, END)
-            txtDisplay.insert(0, string=self.operator + "=" + self.result)
+            txtDisplay.insert(0, string=self.operator + "=" + self.output)
             self.current = 0
         except SyntaxError:
             txtDisplay.delete(0, END)
             txtDisplay.insert(0, self.operator)
+
 
     def oprtrClick(self, op):
         if self.current is 0:
@@ -36,12 +77,16 @@ class Calculator:
             self.operator = self.operator + op
             txtDisplay.delete(0, END)
             txtDisplay.insert(0, string=self.operator)
+            self.isButton = 0
+            self.isRightParen = 0
         else:
             self.operator = self.operator + op
+            self.isButton = 0
+            self.isRightParen = 0
             self.expOutput(self.operator)
 
     def equals(self):
-        self.operator = self.result
+        self.operator = self.output
         txtDisplay.delete(0, END)
         txtDisplay.insert(0, string=self.operator)
 
@@ -53,6 +98,7 @@ class Calculator:
         self.operator = self.operator[: -1]
         txtDisplay.delete(0, END)
         txtDisplay.insert(0, string=self.operator)
+
 
 smartCal = Calculator()
 
@@ -90,10 +136,10 @@ btnDecimal = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 
                    bg="powder blue", command=lambda: smartCal.btnClick("."))
 
 btnLeftParen = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="(",
-                   bg="powder blue", command=lambda: smartCal.btnClick("("))
+                   bg="powder blue", command=smartCal.leftParenthesesClick)
 
 btnRightParen = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text=")",
-                   bg="powder blue", command=lambda: smartCal.btnClick(")"))
+                   bg="powder blue", command=smartCal.rightParenthesesClick)
 
 Add_btn = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="+",
                  bg="powder blue", command=lambda: smartCal.oprtrClick("+"))
@@ -116,30 +162,30 @@ btnClear = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'b
 btnBackspace = Button(cal, padx=16, pady=16, bd=8, fg="black", font=('arial', 20, 'bold'), text="⌫",
                   bg="powder blue", command=smartCal.delete)
 
-# ~*~*~Positioning~*~*~
+# {-|-|-|-Positioning-|-|-|-}
 
 txtDisplay.grid(columnspan=4)
-# =========ROW1================== #
+# =========================== #
 btn7.grid(row=1, column=0)
 btn8.grid(row=1, column=1)
 btn9.grid(row=1, column=2)
 Add_btn.grid(row=1, column=3)
-# =========ROW2================== #
+# =========================== #
 btn4.grid(row=2, column=0)
 btn5.grid(row=2, column=1)
 btn6.grid(row=2, column=2)
 Sub_btn.grid(row=2, column=3)
-# =========ROW3================== #
+# =========================== #
 btn1.grid(row=3, column=0)
 btn2.grid(row=3, column=1)
 btn3.grid(row=3, column=2)
 Mul_btn.grid(row=3, column=3)
-# =========ROW4================== #
+# =========================== #
 btn0.grid(row=4, column=0)
 btnClear.grid(row=4, column=1)
 btnEquals.grid(row=4, column=2)
 Div_btn.grid(row=4, column=3)
-# =========ROW5================== #
+# =========================== #
 btnDecimal.grid(row=5, column=0)
 btnLeftParen.grid(row=5, column=1)
 btnRightParen.grid(row=5, column=2)
